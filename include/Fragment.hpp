b95ed5c80b9db9c64f01bd5552fafbf90987d392
Enzo Renard <60904@etu.he2b.be>
2024-11-19 23:10:41 +0100

    Add Fragment declaration

---
+++ include/Fragment.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Enzo Renard on 19/11/2024.
+//
+
+#ifndef FRAGMENT_HPP
+#define FRAGMENT_HPP
+#include <vector>
+
+#include "Fragment.hpp"
+class Fragment {
+    std::vector<std::vector<Tile>> tiles;
+};
+#endif //FRAGMENT_HPP
